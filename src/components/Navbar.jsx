12acda7bcc1da3598092d4f3f016161b4268e75f
OtavioAleluia <otavio.alelluia@gmai.com>
2024-09-16 18:00:50 -0300

    criação de Navbar com icones de placeholder.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='fixed top-0 w-screen bg-blue-700'>
-      
+    <div className='fixed top-0 w-screen bg-slate-400 text-white h-16 m-0 flex flex-row shadow-md'>
+      <i>1</i>
+      <i>2</i>
+      <i>3</i>
+      <i>4</i>
+      <i>5</i>
     </div>
   )
 }
