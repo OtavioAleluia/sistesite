84c913481c6bdbdcd7ddee31eac895550410fddd
OtavioAleluia <otavio.alelluia@gmai.com>
2024-09-19 17:55:41 -0300

    mudanças na cor navBar e fonte do texto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className='fixed top-0 w-screen bg-slate-400 text-white h-16 m-0 flex flex-row shadow-md'>
-      <i>1</i>
-      <i>2</i>
-      <i>3</i>
-      <i>4</i>
-      <i>5</i>
+    
+    <div className='fixed top-0 w-screen bg-blue-950 text-white h-16 m-0  items-center justify-center px-4 shadow-md'>
+      <div className='items-center w-screen justify-center'>
+        {/* Logo */}
+        <div className='flex items-center justify-center'>
+          <img src="logo.png" alt="Logo" className='h-10' />
+        </div>
+
+      </div>
+
+      {/* Menu */}
+      <div className='flex space-x-7 justify-center mx-4'>
+        <i className='cursor-pointer hover:text-gray- font-[Libre-franklin]'>Home</i>
+        <i className='cursor-pointer hover:text-gray- font-[Libre-franklin]'>Sobre</i>
+        <i className='cursor-pointer hover:text-gray- font-[Libre-franklin]'>Serviços</i>
+        <i className='cursor-pointer hover:text-gray- font-[Libre-franklin]'>Contato</i>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
+
